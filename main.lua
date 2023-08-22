c96c14bd84255ff57abe4dff473e651f572594b5
byronlove-lace <byronlovelace@proton.me>
2023-08-22 20:06:13 +0700

    added boarders

--- main.lua
+++ main.lua
@@ -9,9 +9,14 @@ function love.load()
         _G.pacman = {}
         pacman.x = 100
         pacman.y = 200
+        pacman.mouth = {27.5, 332.5}
 
-        x = 10 
-        y = 10 + x
+        _G.boarder = {
+                left = {1, 1, 1, 600},
+                right = {800, 1, 800, 600},
+                down = {1, 600, 800, 600},
+                up = {1, 1, 800, 1}
+        }
 
         _G.food = {
                 x = 600,
@@ -19,13 +24,13 @@ function love.load()
                 eaten = false
         }
 
-        pacman.mouth = {27.5, 332.5}
 
         love.graphics.setBackgroundColor(0.5, 0.5, 1, 1)
 end
 
 function love.update(dt)
         number = number + 1
+
 --[[
         if pacman.mouth[1] == open_start then
                 pacman.mouth[1] = closed_start
@@ -37,29 +42,57 @@ function love.update(dt)
 --]]
 
         if love.keyboard.isDown("d") then
-                pacman.x = pacman.x + 3
                 pacman.mouth[1] = 27.5 
                 pacman.mouth[2] = 332.5 
-                
+
+                if pacman.x >= 750 then
+                        pacman.x = 750
+                else
+                        pacman.x = pacman.x + 3
+                end
         end
+
         if love.keyboard.isDown("a") then
-                pacman.x = pacman.x - 3
                 pacman.mouth[1] = 27.5 + 180
                 pacman.mouth[2] = 332.5 + 180
+
+                if pacman.x <= 50 then
+                        pacman.x = 50
+                else
+                        pacman.x = pacman.x - 3
+                end
+
         end
         if love.keyboard.isDown("w") then
-                pacman.y = pacman.y - 3
                 pacman.mouth[1] = 27.5 + 270
                 pacman.mouth[2] = 332.5 + 270
+
+                if pacman.y <= 50 then
+                        pacman.y = 50
+                else
+                        pacman.y = pacman.y - 3
+                end
         end
+
         if love.keyboard.isDown("s") then
-                pacman.y = pacman.y + 3
                 pacman.mouth[1] = 27.5 + 90
                 pacman.mouth[2] = 332.5 + 90
+
+                if pacman.y >= 550 then
+                        pacman.y = 550
+                else
+                        pacman.y = pacman.y + 3
+                end
         end
 end
 
 function love.draw()
+
+        love.graphics.line(boarder.left)
+        love.graphics.line(boarder.right)
+        love.graphics.line(boarder.up)
+        love.graphics.line(boarder.down)
+
         if not food.eaten then
                 love.graphics.setColor(1, 1, 1)
                 love.graphics.rectangle("fill", food.x, food.y, 35, 35)
@@ -73,6 +106,7 @@ function love.draw()
 
         love.graphics.print(number)
         love.graphics.setColor(252/255, 243/255, 63/255, 1)
+
         love.graphics.arc("fill", 
         "pie", 
         pacman.x, 
